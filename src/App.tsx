f2e2b0cf41b3a06f72bc280af2c80a87b0bfea20
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-05-31 15:42:52 -0400

    feat: implementation routes in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import AddressForm from './pages/Register';
-import { Address } from './pages/Register';
 import { Header } from './components/Header';
-import { Home } from './pages/Home';
+import { AppRoutes } from './routes/routes';
 
 const RegisterAddress: React.FC = () => {
-  const handleRegister = (address: Address) => {
-    console.log('Endereço cadastrado:', address);
-    // Lógica para salvar o endereço no banco de dados
-  };
-
   return (
     <div>
       <Header />
       <main className='w-full flex items-center justify-center '>
-        <Home />
+        <AppRoutes />
       </main>
-      {/* <h2>Cadastro de Endereço</h2> */}
-      {/* <AddressForm onSubmit={handleRegister} /> */}
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header className="bg-black text-white h-28 flex items-center pl-5">
-            <p className="font-IBM font-bold text-3xl">SWITCHMARS</p>
+            <p className="font-IBM font-bold text-3xl cursor-pointer">SWITCHMARS</p>
         </header>
     )
 }
